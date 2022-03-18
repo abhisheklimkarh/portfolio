7005de18ce627e4434ae6c8fbeecb4c85f6da5c7
Abhishek Limkar <limkarabhishek@gmail.com>
2022-03-18 14:08:15 +0530

    second commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import django_heroku
 
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 # Added Static File Dirs Manually
 # Added Manually
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
